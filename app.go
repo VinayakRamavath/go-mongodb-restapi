db8b8ca026d6b51d064d615e9c3106e0117d7af2
Vinayak <vinayak@zoomtail.com>
2019-04-11 00:17:22 +0530

    first commit

---
+++ app.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	. "demo-app/dao"
+	. "demo-app/models"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var dao = MoviesDAO{}
+
+// GET list of movies
+func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
+
+	movies, err := dao.FindAll()
+
+	if err != nil {
+		fmt.Println("inside err")
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, movies)
+}
+
+// GET a movie by its ID
+func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	movie, err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, movie)
+}
+
+// POST a new movie
+func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	movie.ID = bson.NewObjectId()
+	if err := dao.Insert(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, movie)
+}
+
+// PUT update an existing movie
+func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Update(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+// DELETE an existing movie
+func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Delete(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func init() {
+
+	dao.Server = "localhost"
+	dao.Database = "movies_db"
+	dao.Connect()
+}
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
+	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ dao/dao.go
@@ -0,0 +1,64 @@
+package dao
+
+import (
+	. "demo-app/models"
+	"fmt"
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type MoviesDAO struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+const (
+	COLLECTION = "movies"
+)
+
+func (m *MoviesDAO) Connect() {
+
+	session, err := mgo.Dial(m.Server)
+	if err != nil {
+		fmt.Println("failed to connect with moongodb")
+		log.Fatal(err)
+	}
+	fmt.Println("established connection with mongodb at", m.Server)
+	db = session.DB(m.Database)
+}
+
+func (m *MoviesDAO) FindAll() ([]Movie, error) {
+
+	var movies []Movie
+	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
+	return movies, err
+}
+
+func (m *MoviesDAO) FindById(id string) (Movie, error) {
+
+	var movie Movie
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
+	return movie, err
+}
+
+func (m *MoviesDAO) Insert(movie Movie) error {
+
+	err := db.C(COLLECTION).Insert(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Delete(movie Movie) error {
+
+	err := db.C(COLLECTION).Remove(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Update(movie Movie) error {
+
+	err := db.C(COLLECTION).UpdateId(movie.ID, &movie)
+	return err
+}
